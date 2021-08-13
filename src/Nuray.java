080de328af7bd5bfbc025aa853294b34c92dda05
nsari <88260442+nsari@users.noreply.github.com>
2021-08-12 21:41:36 -0500

    Update Nuray.java
    created code

--- src/Nuray.java
+++ src/Nuray.java
@@ -2,9 +2,9 @@ import org.w3c.dom.ls.LSOutput;
 
 public class Nuray {
     public static void main(String[] args) {
+        System.out.println("Good Job");
         System.out.println("nuray");
-
-
+        System.out.println("nuray");
 
     }
 }
